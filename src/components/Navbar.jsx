c550cc7af5c9f58f50c5c96b982598535c8646c1
iamchacha021 <iamchacha021@gmail.com>
2023-03-24 21:20:28 +0300

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return ( 
+        <div>
+            <nav className="navbar bg-dark" data-bs-theme="dark">
+                <button>Home</button>
+            </nav>
+        </div>
+     );
+}
+ 
+export default Navbar;
